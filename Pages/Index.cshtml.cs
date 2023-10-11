af4ac6025d9568939da88f265293b67adafada88
james hill <james.hill@challenger.com>
2023-10-11 02:50:18 -0500

    update to the Ai model

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace AIChat.Pages
 {
     public class IndexModel : PageModel
+
+        
     {
         private readonly ILogger<IndexModel> _logger;
 
@@ -15,6 +17,16 @@ namespace AIChat.Pages
         public void OnGet()
         {
 
+        }
+        // list of all the videos
+        public List<Video> Videos { get; set; } = new List<Video>();
+        // list of all the videos
+        //url of the video, description and title of video
+        public class Video
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string VideoUrl { get; set; }
         }
     }
 }
